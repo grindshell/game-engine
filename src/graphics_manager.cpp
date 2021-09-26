ff9e5da2e2fa5bbe7549916035f833b3ffc4e404
grindshell <grindshell@protonmail.com>
2021-09-25 22:47:47 -0400

    render geometry

--- src/graphics_manager.cpp
+++ src/graphics_manager.cpp
@@ -44,6 +44,12 @@ bool GraphicsManager::initializeSDL() {
 bool GraphicsManager::createWindow() {
 	bool success = true;
 
+	//Set texture filtering to linear
+	if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+	{
+		printf( "Warning: Linear texture filtering not enabled!" );
+	}
+
 	gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 	if( gWindow == NULL )
 	{
@@ -52,7 +58,7 @@ bool GraphicsManager::createWindow() {
 	}
 	else
 	{
-		            //Create renderer for window
+		//Create renderer for window
         gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
         if( gRenderer == NULL )
         {
@@ -78,14 +84,41 @@ bool GraphicsManager::createWindow() {
 }
 
 bool GraphicsManager::render() {
+	// From rendering png:
 	//Clear screen
-    SDL_RenderClear( gRenderer );
-
+    // SDL_RenderClear( gRenderer );
     //Render texture to screen
-    SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
-
+    // SDL_RenderCopy( gRenderer, gTexture, NULL, NULL );
     //Update screen
-    SDL_RenderPresent( gRenderer );
+    // SDL_RenderPresent( gRenderer );
+
+	//Clear screen
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+	SDL_RenderClear( gRenderer );
+
+	//Render red filled quad
+	SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );		
+	SDL_RenderFillRect( gRenderer, &fillRect );
+
+	//Render green outlined quad
+	SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+	SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );		
+	SDL_RenderDrawRect( gRenderer, &outlineRect );
+	
+	//Draw blue horizontal line
+	SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0xFF, 0xFF );		
+	SDL_RenderDrawLine( gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
+
+	//Draw vertical line of yellow dots
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0x00, 0xFF );
+	for( int i = 0; i < SCREEN_HEIGHT; i += 4 )
+	{
+		SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
+	}
+
+	//Update screen
+	SDL_RenderPresent( gRenderer );
 
     printf("render");
 
@@ -96,12 +129,12 @@ bool GraphicsManager::loadAssets() {
     bool success = true;
 
     //Load PNG texture
-    gTexture = loadTexture( "texture.png" );
-    if( gTexture == NULL )
-    {
-        printf( "Failed to load texture image!\n" );
-        success = false;
-    }
+    // gTexture = loadTexture( "texture.png" );
+    // if( gTexture == NULL )
+    // {
+    //     printf( "Failed to load texture image!\n" );
+    //     success = false;
+    // }
 
 	return success;
 }
