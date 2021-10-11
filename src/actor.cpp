80f59fd7aa633a9aaeff95057e9369c1dd028358
grindshell <grindshell@protonmail.com>
2021-10-11 12:17:41 -0400

    iterate through actor container in game loop

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <string>
 
 #include "actor.h"
 
-Actor::Actor(int x, int y)
+std::string Actor::to_string()
 {
-	this->setXY(x, y);
+	return "(" + std::to_string(this->x) + ", " + std::to_string(this->y) + ")";
 }
 
-void Actor::setXY(int x, int y)
+Actor::Actor(int x, int y)
+{
+	this->set_xy(x, y);
+}
+
+void Actor::set_xy(int x, int y)
 {
 	this->x = x;
 	this->y = y;
--- src/game_core.cpp
+++ src/game_core.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
@@ -15,10 +16,15 @@ void GameCore::run ()
     running = true;
 
     SDL_Event e;
+	std::list<Actor>::iterator ait;
 
     while(running) {
 		while( SDL_PollEvent( &e ) != 0 )
 		{
+			for (ait = actors.begin(); ait != actors.end(); ++ait){
+				std::cout << ait->to_string() << "\n" << std::flush;
+			}
+
 			if( e.type == SDL_QUIT )
 			{
 				running = false;
