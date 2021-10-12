9d23fb5835ec34e8b5e6d45214f12f9c2d474f50
grindshell <grindshell@protonmail.com>
2021-10-11 21:58:00 -0400

    Put height and width on actor class.

--- src/actor.cpp
+++ src/actor.cpp
@@ -8,13 +8,25 @@ std::string Actor::to_string()
 	return "(" + std::to_string(this->x) + ", " + std::to_string(this->y) + ")";
 }
 
-Actor::Actor(int x, int y)
+Actor::Actor(int x, int y, int height, int width)
 {
 	this->set_pos(x, y);
+	this->set_height(height);
+	this->set_width(width);
 }
 
 void Actor::set_pos(int x, int y)
 {
 	this->x = x;
 	this->y = y;
+}
+
+void Actor::set_height(int height)
+{
+	this->height = height;
+}
+
+void Actor::set_width(int width)
+{
+	this->width = width;
 }
--- src/actor.h
+++ src/actor.h
@@ -3,10 +3,14 @@
 class Actor {
     int x = 0;
     int y = 0;
+    int height = 0;
+    int width = 0;
 public:
-    Actor(int x, int y);
+    Actor(int x, int y, int height, int width);
 
     void set_pos(int x, int y);
+    void set_height(int width);
+    void set_width(int height);
     std::string to_string();
 };
 #endif
